8b9feda39e20fd014a4c5f386ade2eca03d51b03
jose.deharo.intern <josedeharojimenez1@gmail.com>
2026-03-04 15:06:48 +0100

    Implementacion en database.config datos env

--- backend/src/databases/database.config.js
+++ backend/src/databases/database.config.js
@@ -2,8 +2,8 @@ import dotenv from 'dotenv';
 dotenv.config(); 
 
 export default configDataPool = {
-    host: process.env.DB_host,
-    user: "",
-    password: "",
-    database: ""
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 };
