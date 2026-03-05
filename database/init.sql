b1353129005c18e4296810e2073cb4b92332065e
jose.deharo.intern <josedeharojimenez1@gmail.com>
2026-03-05 23:06:02 +0100

    Implementación de tabla follow, post

--- database/init.sql
+++ database/init.sql
@@ -14,7 +14,20 @@ create table if not exists user(
 
 -- TODO Implementar las demás tablas. 
 
+create table if not exists follow(
+    follower_id bigint,
+    followed_id bigint,
+    PRIMARY KEY (follower_id, followed_id), 
+    constraint fk_follower_user FOREIGN KEY (follower_id) references user(user_id) on delete cascade, 
+    constraint fk_followed_user FOREIGN KEY (followeb_id) references user(user_id) on delete cascade 
+); 
 
-
-
+create table if not exists post(
+    post_id bigint primary key AUTO_INCREMENT, 
+    user_id bigint not null,
+    title varchar(200), 
+    description_post varchar(500), 
+    created_at  timestamp default current_timestamp, 
+    constraint fk_post_user FOREIGN key (user_id) references user(user_id) on delete cascade
+); 
 
